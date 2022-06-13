cebfba07f18e285690e6b4ad422c486506104f11
S <scottlydon18@gmail.com>
2022-06-12 19:07:57 -0700

    Add Figma methods.

---
+++ Sources/APIHelpers/Figma/String.swift
@@ -0,0 +1,19 @@
+//
+//  String.swift
+//  APIs
+//
+//  Created by Scott Lydon on 6/12/22.
+//
+
+import Foundation
+
+
+extension String {
+
+    /// Gets the url string for attempting to retrieve your Figma board as JSON.
+    /// - Parameter board: your board's id
+    /// - Returns: returns the url string.
+    static func figma(board: String) -> String {
+        "https://api.figma.com/v1/files/" + board
+    }
+}
