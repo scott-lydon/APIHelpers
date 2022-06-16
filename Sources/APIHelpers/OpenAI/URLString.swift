bf5196ec5c5c9537c23a0a2d841a6ff8e1925c09
S <scottlydon18@gmail.com>
2022-06-15 18:44:09 -0700

    Fix public extension conforming bug.

--- Sources/APIHelpers/OpenAI/URLString.swift
+++ Sources/APIHelpers/OpenAI/URLString.swift
@@ -11,8 +11,8 @@ public struct URLString: Equatable {
     var string: String
 }
 
-public extension URLString: ExpressibleByStringLiteral {
-    init(stringLiteral value: StringLiteralType) {
+extension URLString: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) {
         string = value.replacingOccurrences(of: "_", with: "-")
     }
 }
